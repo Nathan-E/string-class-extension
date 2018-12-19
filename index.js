24cd0f63b3d2cbf2bc8a882e506d52f626a275ea
Chibueze <eziokwubunduchibueze@gmail.com>
2018-12-19 23:22:41 +0100

    WIP hasVowel Function

---
+++ index.js
@@ -0,0 +1,7 @@
+// hasVowels (typeof Boolean): Returns true if the string contains vowels. 
+// This method implements Regular Expression.
+String.prototype.hasVowel = () => {
+    
+}
+
+module.export =  'String'; 
