bb92a5c14300c318629abfd7af3dd84f2b0af1fc
Chibueze <eziokwubunduchibueze@gmail.com>
2018-12-19 23:51:56 +0100

    toUpper Function added to index.js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 // This method implements Regular Expression.
 String.prototype.hasVowels = function () {
     return /[aeiou]/i.test(this); 
+};
+
+// toUpper (typeof String): Returns the String in question but with all characters in upper
+// cases as applicable. This method should be implemented without the use of the
+// toUpperCase internal methods.
+
+String.prototype.toUpper = function () {
+    const check = /[a-z]/;
+    let result = '';
+    for(let i = 0; i<this.length; i++){
+      result += this[i].replace(check, String.fromCharCode(this[i].charCodeAt(0)-32));
+    }
+    return result;
   }
 
 
