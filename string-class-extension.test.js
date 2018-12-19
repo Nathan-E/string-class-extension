058952e2eb06dacb574c600e7d2ea39d72570e42
Chibueze <eziokwubunduchibueze@gmail.com>
2018-12-19 23:47:31 +0100

    WIP test cases for toUpper function

--- string-class-extension.test.js
+++ string-class-extension.test.js
@@ -1,5 +1,6 @@
 require('./index');
 
+//test for hasVowel function
 describe('Testing if a string has Vowels', () => {
    test('A simple case where no vowel exist', () => {
        expect('dry'.hasVowels()).toBeFalsy();
@@ -13,4 +14,11 @@ describe('Testing if a string has Vowels', () => {
     test('A simple case of empty string', () => {
        expect(''.hasVowels()).toBeFalsy();
     });
+});
+
+//test for toUpper function
+describe('Changes the case of a string to Uppercase', () => {
+  test('A simple case of lowercase letters of a string ', () => {
+      expect('dry'.hasVowels()).toBe('DRY');
+  });
 });
