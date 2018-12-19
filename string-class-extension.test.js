f4497aeae1fd0c5dd932da5433a50de8322bcfa2
Chibueze <eziokwubunduchibueze@gmail.com>
2018-12-19 23:58:06 +0100

    WIP test cases for toLower function

--- string-class-extension.test.js
+++ string-class-extension.test.js
@@ -19,15 +19,25 @@ describe('Testing if a string has Vowels', () => {
 //test for toUpper function
 describe('Changes the case of a string to Uppercase', () => {
   test('A simple case of all lowercase letters of a string ', () => {
-      expect('dry'.hasVowels()).toBe('DRY');
+      expect('dry'.toUpper()).toBe('DRY');
   });
   test('A  case of all uppercase letters of a string ', () => {
-      expect('HOME'.hasVowels()).toBe('HOME');
+      expect('HOME'.toUpper()).toBe('HOME');
   });
   test('A  case of mixed case letters of a string ', () => {
-      expect('DEcaGOn'.hasVowels()).toBe('DECAGON');
+      expect('DEcaGOn'.toUpper()).toBe('DECAGON');
   });
   test('A  case of non letters and letters ', () => {
-      expect('123ab'.hasVowels()).toBe('123AB');
+      expect('123ab'.toUpper()).toBe('123AB');
+  });
+});
+
+//test for toLower function
+describe('Changes the case of a string to Uppercase', () => {
+  test('A simple case of all lowercase letters of a string ', () => {
+      expect('dry'.toLower()).toBe('dry');
+  });
+  test('A  case of all uppercase letters of a string ', () => {
+      expect('HOME'.toLower()).toBe('home');
   });
 });
