bba16cf1f4e6f1cbd3cb5b7be6e070d36f507858
Chibueze <eziokwubunduchibueze@gmail.com>
2018-12-20 12:28:17 +0100

    Reade.md file added

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ String.prototype.hasVowels = function () {
   return /[aeiou]/i.test(this);
 };
 
+
 // toUpper (typeof String): Returns the String in question but with all characters in upper
 // cases as applicable. This method was implemented without the use of the
 // toUpperCase internal methods.
-
 String.prototype.toUpper = function () {
   //the regex literal /[a-z]/ searches for alphabet between a to z
   const check = /[a-z]/;
@@ -21,9 +21,9 @@ String.prototype.toUpper = function () {
   return result;
 };
 
+
 // toLower (typeof String): Returns the String in question but with all characters in their lowercases as applicable. 
 // This method was implemented without the use of the toLowerCase internal methods.
-
 String.prototype.toLower = function () {
   //the regex literal /[A-Z]/ searches for alphabet between A to Z
   const check = /[A-Z]/;
@@ -36,12 +36,14 @@ String.prototype.toLower = function () {
   return result;
 };
 
+
 // ucFirst (typeof String): Returns the String in question but changes the First Character to an Upper case. 
 // the toUpper method above was implemented in this method.
 String.prototype.ucFirst = function () {
   //makes use of the toUpper function above to change the first character in a string to Uppercase
   return (this[0].toUpper() + this.substring(1, this.length));
 };
+
 
 // isQuestion (typeof Boolean): Return true if the string is a question (ending with a question
 // mark). This method implements Regular Expression.
@@ -50,6 +52,7 @@ String.prototype.isQuestion = function () {
   return /\?$/.test(this);
 };
 
+
 // words: Returns a list of the words in the string, as an Array. This method  implements Regular Expression.
 String.prototype.words = function () {
   //the regex literals searches for words in a sentence/string, then the match method return the words in an array
@@ -57,17 +60,18 @@ String.prototype.words = function () {
 };
 
 // wordCount (typeof Number): Returns the number of words in the string. It makes use of the words method above
-
 String.prototype.wordCount = function () {
   //uses the words function above and return the number of words in the array
   return this.words().length;
 };
+
 
 // fromCurrency (typeof Number): Returns a number representation of the Currency String e.g 11,111.11 return 11111.11
 String.prototype.fromCurrency = function () {
   //the regex literal removes comma sign from a number
   return this.replace(/\,/g, '');
 };
+
 
 // inverseCase (typeof String): Returns each letter in the string as an inverse of its current case e.g Mr. Ben returns mR. bEN.
 String.prototype.inverseCase = function () {
@@ -80,6 +84,7 @@ String.prototype.inverseCase = function () {
   return result;
 };
 
+
 // aternatinglCase (typeof String): Returns the letters in alternating cases. It starts with a lower case e.g Onomatopoeia should return oNoMaToPoEiA.
 String.prototype.alternatingCase = function () {
   let result = this[0].toLower();
@@ -91,8 +96,8 @@ String.prototype.alternatingCase = function () {
   return result;
 };
 
-// numberWords (typeof String): Returns the numbers in words e.g 325 should return three two five.
 
+// numberWords (typeof String): Returns the numbers in words e.g 325 should return three two five.
 String.prototype.numberWords = function () {
   const match = {
     0: 'zero ',
@@ -114,6 +119,7 @@ String.prototype.numberWords = function () {
   }
   return result.trim();
 };
+
 
 // isDigit (typeof Boolean): Returns true if the string is a digit(one number) e.g 3 should return true and 34 should return false. 
 // This method implements Regular Expression.
